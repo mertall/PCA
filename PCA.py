b43f277fab75bb355924cc2e6ec18429068743db
Mridul Sarkar <mridul1399@gmail.com>
2019-12-11 20:09:40 -0800

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -4,11 +4,11 @@ from scipy.sparse import rand
 import numpy as np
 from matplotlib import pyplot
 
-# Create sparse 3 dimensional matrix with 1random values, random state has been fixed to ensure the results are replicable
+# Create sparse 3 dimensional matrix with 1random values
 
-X = rand(100, 3, density=0.05)
+X = abs(rand(10000, 3, density=0.1))
 
-# Preform SVD, both GESDD and GESVD will be used to later determine which algorithm will be better for the eventual data that will be processed
+# Preform truncated SVD, both GESDD and GESVD will be used to later determine which algorithm will be better for the eventual data that will be processed
 
 U, S, V = svds(X,k=2)
 
@@ -19,6 +19,7 @@ fig = pyplot.figure()
 ax = pyplot.axes(projection='3d')
 ax = pyplot.axes(projection='3d')
 ax.plot3D(singular_values[:,0],singular_values[:,1], 'green')
+print(singular_values)
 pyplot.show()
 
 
